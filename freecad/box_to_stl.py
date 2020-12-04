b295b078275d86fe5ef42051d6e3d62851bfbec1
Alicia Key <akey7@users.noreply.github.com>
2020-12-04 09:43:34 -0700

    FreeCAD Support
    Creating two spheres.

--- freecad/box_to_stl.py
+++ freecad/box_to_stl.py
@@ -2,8 +2,15 @@ import FreeCAD
 import Part
 import Mesh
 
-shape = Part.makeSphere(1)
 doc = App.newDocument("Doc")
-pf = doc.addObject("Part::Feature", "myShape")
-pf.Shape = shape
-Mesh.export([pf], "bar.stl")
+
+shape1 = Part.makeSphere(1)
+pf1 = doc.addObject("Part::Feature", "myShape1")
+pf1.Shape = shape1
+
+shape2 = Part.makeSphere(1)
+shape2.translate((10, 10, 10))
+pf2 = doc.addObject("Part::Feature", "myShape2")
+pf2.Shape = shape2
+
+Mesh.export([pf1, pf2], "bar.stl")
