1270e00f73abf4a5e6e4da8b5588042a3fe3a403
Alicia Key <akey7@users.noreply.github.com>
2020-12-04 11:09:02 -0700

    FreeCAD Support
    Create a translation matrix.

--- freecad/box_to_stl.py
+++ freecad/box_to_stl.py
@@ -13,4 +13,11 @@ shape2.translate((10, 10, 10))
 pf2 = doc.addObject("Part::Feature", "myShape2")
 pf2.Shape = shape2
 
-Mesh.export([pf1, pf2], "bar.stl")
+m = FreeCAD.Matrix()
+m.move((2, 2, 2))
+shape3 = Part.makeSphere(1)
+shape3.transformShape(m)
+pf3 = doc.addObject("Part::Feature", "myShape3")
+pf3.Shape = shape3
+
+Mesh.export([pf1, pf2, pf3], "bar.stl")
