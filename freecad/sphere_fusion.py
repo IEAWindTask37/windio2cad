9c4d43a08f31ec4d76d728a273a9c496ef665bff
Alicia Key <akey7@users.noreply.github.com>
2020-12-04 13:37:07 -0700

    FreeCAD support
    Only need to track Part::Features.

--- freecad/sphere_fusion.py
+++ freecad/sphere_fusion.py
@@ -6,7 +6,6 @@ doc = App.newDocument("Doc")
 
 radii = [1.1, 1.2, 1.3, 1.4, 1.5, 1.4, 1.3, 1.2, 1.1]
 
-union = []
 part_features = []
 for i, r in enumerate(radii):
     shape_name = f"shape_{i}"
@@ -14,7 +13,6 @@ for i, r in enumerate(radii):
     shape.translate((0, 0, i))
     part_feature = doc.addObject("Part::Feature", shape_name)
     part_feature.Shape = shape
-    union.append(shape)
     part_features.append(part_feature)
 
 Mesh.export(part_features, "foo.stl")
