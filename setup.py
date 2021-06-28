69c65ffd8527dc4717498348c152936a19305e35
Alicia Key <Alicia.Key@nrel.gov>
2021-06-28 11:53:52 -0600

    Update setup.py
    Reformatted setup.py with black, mostly to ensure that black still works after a regex warning.

--- setup.py
+++ setup.py
@@ -4,23 +4,23 @@ from setuptools import setup, find_packages
 
 # Top-level setup
 setup(
-    name             = 'windio2cad',
-    version          = '0.0.1',
-    description      = 'windio2cad',
-    long_description =  '''windio2cad takes windio ontology YAML files and translates them into .stl files.''',
-    url              = 'https://github.com/WISDEM/WISDEM',
-    author           = 'NREL WISDEM Team',
-    author_email     = 'systems.engineering@nrel.gov',
-    install_requires = [
-        'pyyaml',
-        'scipy',
-        'matplotlib',
-        'numpy',
-        'ruamel.yaml',
-        'solidpython',
+    name="windio2cad",
+    version="0.0.1",
+    description="windio2cad",
+    long_description="""windio2cad takes windio ontology YAML files and translates them into .stl files.""",
+    url="https://github.com/WISDEM/WISDEM",
+    author="NREL WISDEM Team",
+    author_email="systems.engineering@nrel.gov",
+    install_requires=[
+        "pyyaml",
+        "scipy",
+        "matplotlib",
+        "numpy",
+        "ruamel.yaml",
+        "solidpython",
     ],
-    python_requires  = '>=3.8',
-    packages         = find_packages(exclude=['docs', 'tests', 'ext']),
-    license          = 'Apache License, Version 2.0',
-    zip_safe         = False
+    python_requires=">=3.8",
+    packages=find_packages(exclude=["docs", "tests", "ext"]),
+    license="Apache License, Version 2.0",
+    zip_safe=False,
 )
